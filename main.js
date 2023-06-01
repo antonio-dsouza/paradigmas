0c1748a130daf39977a326772b8c9204fe0790b6
Antônio de Souza <03343199001@canoas.unilasalle.edu.br>
2023-06-01 19:06:31 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    console.log('teste')
+})
+
+document.addEventListener("DOMContentLoaded", function(e) {
+    function calculateTotal() {
+        const entradas = document.getElementById("entradas");
+    
+        console.log(entradas);
+    }
+    
+    calculateTotal();
+});
